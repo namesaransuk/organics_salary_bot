a49f1e24fcc7d1362722ec906b22125a878b5549
namesaransuk <namesaransuk@gmail.com>
2022-11-18 16:50:43 +0700

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -7,69 +7,110 @@
 //               \/
 
 
-  $LINEData = file_get_contents('php://input');
-  $jsonData = json_decode($LINEData,true);
+$LINEData = file_get_contents('php://input');
+$jsonData = json_decode($LINEData, true);
 
-  $replyToken = $jsonData["events"][0]["replyToken"];
-  $userID = $jsonData["events"][0]["source"]["userId"];
-  $text = $jsonData["events"][0]["message"]["text"];
-  $timestamp = $jsonData["events"][0]["timestamp"];
+$replyToken = $jsonData["events"][0]["replyToken"];
+$userID = $jsonData["events"][0]["source"]["userId"];
+$text = $jsonData["events"][0]["message"]["text"];
+$timestamp = $jsonData["events"][0]["timestamp"];
 
-  // $servername = "https://d588-184-82-148-66.ap.ngrok.io/phpmyadmin/";
-  $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "LINE";
-  $mysql = new mysqli($servername, $username, $password, $dbname);
-  mysqli_set_charset($mysql, "utf8");
+// $servername = "https://d588-184-82-148-66.ap.ngrok.io/phpmyadmin/";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "LINE";
+$mysql = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($mysql, "utf8");
 
-  if ($mysql->connect_error){
+if ($mysql->connect_error) {
   $errorcode = $mysql->connect_error;
-  print("MySQL(Connection)> ".$errorcode);
-  }
+  print("MySQL(Connection)> " . $errorcode);
+}
 
-  function sendMessage($replyJson, $sendInfo){
-          $ch = curl_init($sendInfo["URL"]);
-          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-          curl_setopt($ch, CURLOPT_POST, true);
-          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-              'Content-Type: application/json',
-              'Authorization: Bearer ' . $sendInfo["AccessToken"])
-              );
-          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
-          $result = curl_exec($ch);
-          curl_close($ch);
-    return $result;
-  }
+function sendMessage($replyJson, $sendInfo)
+{
+  $ch = curl_init($sendInfo["URL"]);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+  curl_setopt($ch, CURLOPT_POST, true);
+  curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    array(
+      'Content-Type: application/json',
+      'Authorization: Bearer ' . $sendInfo["AccessToken"]
+    )
+  );
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  return $result;
+}
 
-  $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
+$mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
 
-  $getUser = $mysql->query("SELECT * FROM `Customer` WHERE `UserID`='$userID'");
-  $getuserNum = $getUser->num_rows;
-  $replyText["type"] = "text";
-  if ($getuserNum == "0"){
-    $replyText["text"] = "สวัสดีคับบบบ";
-  } else {
-    while($row = $getUser->fetch_assoc()){
-      $Name = $row['Name'];
-      $Surname = $row['Surname'];
-      $CustomerID = $row['CustomerID'];
+// INSERT INTO `customer` (`UserID`, `CustomerID`, `Name`, `Surname`) VALUES ('U7d6ab1d8497e4a799721a05c0f0458d3', 'IT0231', 'Saransuk', 'Yimyong');
+
+$replyText["type"] = "text";
+if ($text) {
+  $messageFromUser = trim($text);
+  $checkRegister = explode("รหัส:", $messageFromUser);
+  $needToRegister = $checkRegister && $checkRegister[1];
+
+  if ($needToRegister) {
+    $idForRegister = $checkRegister[1];
+    $getUser = $mysql->query("SELECT * FROM `Customer` WHERE `UserID`='$userID'");
+    $getUserNum = $getUser->num_rows;
+    $getCustomer = $mysql->query("SELECT * FROM `Customer` WHERE `CustomerID`='$idForRegister'");
+    $getCustomerNum = $getCustomer->num_rows;
+
+    if ($getUserNum < 1) {
+      if ($getCustomerNum < 1) {
+        $replyText["text"] = "รหัสพนักงานไม่ตรงกับที่มีในระบบ กรุณาลงทะเบียนใหม่อีกครั้ง";
+      } else {
+        $mysql->query("UPDATE `customer` SET `UserID`='$userID' WHERE `CustomerID`='$idForRegister'");
+        $replyText["text"] = "Line ID นี้ได้ลงทะเบียนด้วยรหัส $idForRegister เรียบร้อยแล้ว";
+      }
+    } else {
+      $replyText["text"] = "ไม่สามารถลงทะเบียนซ้ำได้";
     }
-    $replyText["text"] = "สวัสดีคุณ $Name $Surname (#$CustomerID)";
+  } else {
+    switch ($messageFromUser) {
+      case "register":
+        $replyText["text"] = 'กรุณากรอกรหัสพนักงาน เช่น "รหัส:OGN0123"';
+        break;
+      case "salary":
+        if ($getUserNum < 1) {
+          $replyText["text"] = "กรุณาลงทะเบียนก่อนใช้งาน";
+        } else {
+          while ($row = $getUser->fetch_assoc()) {
+            $Name = $row['Name'];
+            $Surname = $row['Surname'];
+            $CustomerID = $row['CustomerID'];
+          }
+          $replyText["text"] = "สวัสดีคุณ $Name $Surname (#$CustomerID)";
+        }
+        break;
+      case "slips":
+        break;
+      default:
+        $replyText["text"] = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse tenetur molestias, nam rem maxime facilis perferendis saepe culpa assumenda, facere voluptatem nisi nobis id eum error delectus necessitatibus at minima!";
+    }
   }
+}
 
-  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
-  $lineData['AccessToken'] = "QyIKS6V+lZOMRnUG2U9X1SVWzXeJZe+pOHjSwtfQOEkiVFPzNRFm0zvSWjtLyOGnDDCGpQ9dFbR/eZ7iw7mWEGrAVjJ5+8PAxZEsCB2+CNPp10pjrVjwbKeRzC5aMEKfl1kGJlr4nguXsoVVhMGfaQdB04t89/1O/w1cDnyilFU=";
+$lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+$lineData['AccessToken'] = "QyIKS6V+lZOMRnUG2U9X1SVWzXeJZe+pOHjSwtfQOEkiVFPzNRFm0zvSWjtLyOGnDDCGpQ9dFbR/eZ7iw7mWEGrAVjJ5+8PAxZEsCB2+CNPp10pjrVjwbKeRzC5aMEKfl1kGJlr4nguXsoVVhMGfaQdB04t89/1O/w1cDnyilFU=";
 
-  $replyJson["replyToken"] = $replyToken;
-  $replyJson["messages"][0] = $replyText;
+$replyJson["replyToken"] = $replyToken;
+$replyJson["messages"][0] = $replyText;
 
-  $encodeJson = json_encode($replyJson);
+$encodeJson = json_encode($replyJson);
 
-  $results = sendMessage($encodeJson,$lineData);
-  echo $results;
-  http_response_code(200);
+$results = sendMessage($encodeJson, $lineData);
+echo $results;
+http_response_code(200);
 
 // --Credit--
 // Medium: https://medium.com/@sirateek
